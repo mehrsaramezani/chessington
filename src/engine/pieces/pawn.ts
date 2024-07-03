3abbfe8b9549c1f94ed2a2416120734e4e255bba
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 16:10:10 +0300

    Test8 pawns can't jump over pieces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -11,13 +11,18 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let currentSquare = board.findPiece(this)
         let availableMoves = new Array(0)
+
         if (this.player == Player.WHITE) {
-            availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col))
-            if (currentSquare.row == 1) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col))
+            let pieceInFront1w = board.getPiece(Square.at(currentSquare.row + 1,currentSquare.col));
+            let pieceInFront2w = board.getPiece(Square.at(currentSquare.row + 2,currentSquare.col));
+            if (pieceInFront1w == undefined) availableMoves.push(Square.at(currentSquare.row + 1, currentSquare.col))
+            if (currentSquare.row == 1 && pieceInFront1w == undefined && pieceInFront2w == undefined) availableMoves.push(Square.at(currentSquare.row + 2, currentSquare.col))
         }
         if (this.player == Player.BLACK) {
-            availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col))
-            if (currentSquare.row == 6) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col))
+            let pieceInFront1b = board.getPiece(Square.at(currentSquare.row - 1,currentSquare.col));
+            let pieceInFront2b = board.getPiece(Square.at(currentSquare.row - 2,currentSquare.col));
+            if (pieceInFront1b == undefined)  availableMoves.push(Square.at(currentSquare.row - 1, currentSquare.col))
+            if (currentSquare.row == 6 && pieceInFront1b == undefined && pieceInFront2b == undefined) availableMoves.push(Square.at(currentSquare.row - 2, currentSquare.col))
         }
         return availableMoves;
     }
