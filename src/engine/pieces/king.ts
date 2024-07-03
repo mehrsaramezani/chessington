1979cb80169454c3d2ac43a3d8f584f5e8b331d1
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 17:32:18 +0300

    Test17 king done

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -12,14 +12,38 @@ export default class King extends Piece {
         let currentSquare = board.findPiece(this)
         let availableMoves = new Array(0)
 
-        if (currentSquare.row + 1 < 8) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col)) // up
-        if (currentSquare.row - 1 >= 0) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col)) // down
-        if (currentSquare.col + 1 < 8) availableMoves.push(new Square(currentSquare.row , currentSquare.col + 1)) // right
-        if (currentSquare.col - 1 >= 0) availableMoves.push(new Square(currentSquare.row, currentSquare.col - 1)) // left
-        if (currentSquare.row + 1 < 8 && currentSquare.col + 1 < 8) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col + 1)) // top-right
-        if (currentSquare.row + 1 < 8 && currentSquare.col - 1 >= 0) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col - 1)) // top-left
-        if (currentSquare.row - 1 >= 0 && currentSquare.col + 1 < 8) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col + 1)) // bottom-right
-        if (currentSquare.row - 1 >= 0 && currentSquare.col - 1 >= 0) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col - 1)) // bottom-left
+        if (currentSquare.row + 1 < 8) {
+            let checkingSquare = Square.at(currentSquare.row + 1, currentSquare.col)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // up
+        if (currentSquare.row - 1 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row - 1, currentSquare.col)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // down
+        if (currentSquare.col + 1 < 8) {
+            let checkingSquare = Square.at(currentSquare.row , currentSquare.col + 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // right
+        if (currentSquare.col - 1 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row, currentSquare.col - 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // left
+        if (currentSquare.row + 1 < 8 && currentSquare.col + 1 < 8) {
+            let checkingSquare = Square.at(currentSquare.row + 1, currentSquare.col + 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // top-right
+        if (currentSquare.row + 1 < 8 && currentSquare.col - 1 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row + 1, currentSquare.col - 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // top-left
+        if (currentSquare.row - 1 >= 0 && currentSquare.col + 1 < 8) {
+            let checkingSquare = Square.at(currentSquare.row - 1, currentSquare.col + 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // bottom-right
+        if (currentSquare.row - 1 >= 0 && currentSquare.col - 1 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row - 1, currentSquare.col - 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        } // bottom-left
 
         return availableMoves;
     }
