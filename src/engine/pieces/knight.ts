f15bb026025eef4693fd36eea9554fdc299c6ea1
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 15:41:44 +0300

    Test6 knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this)
+        let availableMoves = new Array(0)
+
+        // moving top
+        if (currentSquare.col + 1 < 8 && currentSquare.row + 2 < 8) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col + 1))
+        if (currentSquare.col - 1 >= 0 && currentSquare.row + 2 < 8) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col - 1))
+
+        // moving bottom
+        if (currentSquare.col + 1 < 8 && currentSquare.row - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col + 1))
+        if (currentSquare.col - 1 >= 0 && currentSquare.row - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col - 1))
+
+        // moving right
+        if (currentSquare.row + 1 < 8 && currentSquare.col + 2 < 8) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col + 2))
+        if (currentSquare.row - 1 >= 0 && currentSquare.col + 2 < 8) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col + 2))
+
+        // moving left
+        if (currentSquare.row + 1 < 8 && currentSquare.col - 2 >= 0) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col - 2))
+        if (currentSquare.row - 1 >= 0 && currentSquare.col - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col - 2))
+
+        return availableMoves;
+
     }
 }
