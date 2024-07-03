bcda791d35ca570af479c7768a395b4c62955db9
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 16:36:34 +0300

    Test12 pawns cannot move at the end of the board

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -12,13 +12,13 @@ export default class Pawn extends Piece {
         let currentSquare = board.findPiece(this)
         let availableMoves = new Array(0)
 
-        if (this.player == Player.WHITE) {
+        if (this.player == Player.WHITE && currentSquare.row < 7) {
             let pieceInFront1w = board.getPiece(Square.at(currentSquare.row + 1,currentSquare.col));
             let pieceInFront2w = board.getPiece(Square.at(currentSquare.row + 2,currentSquare.col));
             if (pieceInFront1w == undefined) availableMoves.push(Square.at(currentSquare.row + 1, currentSquare.col))
             if (currentSquare.row == 1 && pieceInFront1w == undefined && pieceInFront2w == undefined) availableMoves.push(Square.at(currentSquare.row + 2, currentSquare.col))
         }
-        if (this.player == Player.BLACK) {
+        if (this.player == Player.BLACK && currentSquare.row > 0) {
             let pieceInFront1b = board.getPiece(Square.at(currentSquare.row - 1,currentSquare.col));
             let pieceInFront2b = board.getPiece(Square.at(currentSquare.row - 2,currentSquare.col));
             if (pieceInFront1b == undefined)  availableMoves.push(Square.at(currentSquare.row - 1, currentSquare.col))
