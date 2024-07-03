c6c6a0c9fe1710d2ecf1a068c10ad18de94eb837
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 14:52:43 +0300

    Test4 bishop

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,38 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this)
+        let availableMoves = new Array(0)
+
+        // moving top-right
+        let index = 1;
+        for (let i = currentSquare.row + 1; i < 8; i++){
+            if (currentSquare.col + index < 8) availableMoves.push(new Square(i, currentSquare.col + index))
+            index++
+        }
+
+        // moving top-left
+        index = 1;
+        for (let i = currentSquare.row + 1; i < 8; i++){
+            if (currentSquare.col - index >= 0) availableMoves.push(new Square(i, currentSquare.col - index))
+            index++
+        }
+
+        // moving bottom-right
+        index = 1;
+        for (let i = currentSquare.row - 1; i >= 0; i--){
+            if (currentSquare.col + index < 8) availableMoves.push(new Square(i, currentSquare.col + index))
+            index++
+        }
+
+        // moving bottom-left
+        index = 1;
+        for (let i = currentSquare.row - 1; i >= 0; i--){
+            if (currentSquare.col - index >= 0) availableMoves.push(new Square(i, currentSquare.col - index))
+            index++
+        }
+
+        console.log(availableMoves)
+        return availableMoves;
     }
 }
