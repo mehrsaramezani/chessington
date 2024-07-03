ea8db92782442a474869ad556f4e0e2bbc2c81d1
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 17:26:37 +0300

    Test16 knight done

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -13,20 +14,45 @@ export default class Knight extends Piece {
         let availableMoves = new Array(0)
 
         // moving top
-        if (currentSquare.col + 1 < 8 && currentSquare.row + 2 < 8) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col + 1))
-        if (currentSquare.col - 1 >= 0 && currentSquare.row + 2 < 8) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col - 1))
+        if (currentSquare.col + 1 < 8 && currentSquare.row + 2 < 8) {
+            let checkingSquare = Square.at(currentSquare.row + 2, currentSquare.col + 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+
+        }
+        if (currentSquare.col - 1 >= 0 && currentSquare.row + 2 < 8) {
+            let checkingSquare = Square.at(currentSquare.row + 2, currentSquare.col - 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
 
         // moving bottom
-        if (currentSquare.col + 1 < 8 && currentSquare.row - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col + 1))
-        if (currentSquare.col - 1 >= 0 && currentSquare.row - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col - 1))
+        if (currentSquare.col + 1 < 8 && currentSquare.row - 2 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row - 2, currentSquare.col + 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
+        if (currentSquare.col - 1 >= 0 && currentSquare.row - 2 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row - 2, currentSquare.col - 1)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
 
         // moving right
-        if (currentSquare.row + 1 < 8 && currentSquare.col + 2 < 8) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col + 2))
-        if (currentSquare.row - 1 >= 0 && currentSquare.col + 2 < 8) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col + 2))
+        if (currentSquare.row + 1 < 8 && currentSquare.col + 2 < 8) {
+            let checkingSquare = Square.at(currentSquare.row + 1, currentSquare.col + 2)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
+        if (currentSquare.row - 1 >= 0 && currentSquare.col + 2 < 8) {
+            let checkingSquare = Square.at(currentSquare.row - 1, currentSquare.col + 2)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
 
         // moving left
-        if (currentSquare.row + 1 < 8 && currentSquare.col - 2 >= 0) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col - 2))
-        if (currentSquare.row - 1 >= 0 && currentSquare.col - 2 >= 0) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col - 2))
+        if (currentSquare.row + 1 < 8 && currentSquare.col - 2 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row + 1, currentSquare.col - 2)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
+        if (currentSquare.row - 1 >= 0 && currentSquare.col - 2 >= 0) {
+            let checkingSquare = Square.at(currentSquare.row - 1, currentSquare.col - 2)
+            if (board.getPiece(checkingSquare)?.player != this.player && !(board.getPiece(checkingSquare) instanceof King)) availableMoves.push(checkingSquare)
+        }
 
         return availableMoves;
     }
