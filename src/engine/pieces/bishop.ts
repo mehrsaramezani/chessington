e74bb54290e66eace9c5991cad19ccce24032318
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 16:23:34 +0300

    Test10 bishops can't jump over pieces

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -15,28 +15,44 @@ export default class Bishop extends Piece {
         // moving top-right
         let index = 1;
         for (let i = currentSquare.row + 1; i < 8; i++){
-            if (currentSquare.col + index < 8) availableMoves.push(new Square(i, currentSquare.col + index))
+            if (currentSquare.col + index < 8) {
+                let checkingSquare = Square.at(i, currentSquare.col + index)
+                if (board.getPiece(checkingSquare) != undefined) break
+                availableMoves.push(checkingSquare)
+            }
             index++
         }
 
         // moving top-left
         index = 1;
         for (let i = currentSquare.row + 1; i < 8; i++){
-            if (currentSquare.col - index >= 0) availableMoves.push(new Square(i, currentSquare.col - index))
+            if (currentSquare.col - index >= 0) {
+                let checkingSquare = Square.at(i, currentSquare.col - index)
+                if (board.getPiece(checkingSquare) != undefined) break
+                availableMoves.push(checkingSquare)
+            }
             index++
         }
 
         // moving bottom-right
         index = 1;
         for (let i = currentSquare.row - 1; i >= 0; i--){
-            if (currentSquare.col + index < 8) availableMoves.push(new Square(i, currentSquare.col + index))
+            if (currentSquare.col + index < 8) {
+                let checkingSquare = Square.at(i, currentSquare.col + index)
+                if (board.getPiece(checkingSquare) != undefined) break
+                availableMoves.push(checkingSquare)
+            }
             index++
         }
 
         // moving bottom-left
         index = 1;
         for (let i = currentSquare.row - 1; i >= 0; i--){
-            if (currentSquare.col - index >= 0) availableMoves.push(new Square(i, currentSquare.col - index))
+            if (currentSquare.col - index >= 0) {
+                let checkingSquare = Square.at(i, currentSquare.col - index)
+                if (board.getPiece(checkingSquare) != undefined) break
+                availableMoves.push(checkingSquare)
+            }
             index++
         }
 
