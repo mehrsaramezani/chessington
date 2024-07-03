7f1d85ba62b56d609b6c6ff492157b180e65a2dd
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 13:28:50 +0300

    Second test, pawns move up/down 1/2 row, only on first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -11,8 +11,14 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let currentSquare = board.findPiece(this)
         let availableMoves = new Array(0)
-        if (this.player == Player.WHITE) availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col))
-        if (this.player == Player.BLACK) availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col))
+        if (this.player == Player.WHITE) {
+            availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col))
+            if (currentSquare.row == 1) availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col))
+        }
+        if (this.player == Player.BLACK) {
+            availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col))
+            if (currentSquare.row == 6) availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col))
+        }
         return availableMoves;
     }
 }
