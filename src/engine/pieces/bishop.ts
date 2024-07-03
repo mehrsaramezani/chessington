3093ba3ff6697ac8f4d6adbdf0d01130d01c6f92
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-03 17:12:04 +0300

    Test13 rook done

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -17,7 +18,11 @@ export default class Bishop extends Piece {
         for (let i = currentSquare.row + 1; i < 8; i++){
             if (currentSquare.col + index < 8) {
                 let checkingSquare = Square.at(i, currentSquare.col + index)
-                if (board.getPiece(checkingSquare) != undefined) break
+                if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+                if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                    availableMoves.push(checkingSquare)
+                    break
+                }
                 availableMoves.push(checkingSquare)
             }
             index++
@@ -28,7 +33,11 @@ export default class Bishop extends Piece {
         for (let i = currentSquare.row + 1; i < 8; i++){
             if (currentSquare.col - index >= 0) {
                 let checkingSquare = Square.at(i, currentSquare.col - index)
-                if (board.getPiece(checkingSquare) != undefined) break
+                if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+                if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                    availableMoves.push(checkingSquare)
+                    break
+                }
                 availableMoves.push(checkingSquare)
             }
             index++
@@ -39,7 +48,11 @@ export default class Bishop extends Piece {
         for (let i = currentSquare.row - 1; i >= 0; i--){
             if (currentSquare.col + index < 8) {
                 let checkingSquare = Square.at(i, currentSquare.col + index)
-                if (board.getPiece(checkingSquare) != undefined) break
+                if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+                if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                    availableMoves.push(checkingSquare)
+                    break
+                }
                 availableMoves.push(checkingSquare)
             }
             index++
@@ -50,7 +63,11 @@ export default class Bishop extends Piece {
         for (let i = currentSquare.row - 1; i >= 0; i--){
             if (currentSquare.col - index >= 0) {
                 let checkingSquare = Square.at(i, currentSquare.col - index)
-                if (board.getPiece(checkingSquare) != undefined) break
+                if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+                if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                    availableMoves.push(checkingSquare)
+                    break
+                }
                 availableMoves.push(checkingSquare)
             }
             index++
--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -14,22 +15,38 @@ export default class Rook extends Piece {
 
         for (let i = currentSquare.row + 1; i < 8; i++) {
             let checkingSquare = Square.at(i, currentSquare.col)
-            if (board.getPiece(checkingSquare) != undefined) break
+            if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+            if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                availableMoves.push(checkingSquare)
+                break
+            }
             availableMoves.push(checkingSquare)
         }
         for (let i = currentSquare.row - 1; i >= 0; i--) {
             let checkingSquare = Square.at(i, currentSquare.col)
-            if (board.getPiece(checkingSquare) != undefined) break
+            if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+            if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                availableMoves.push(checkingSquare)
+                break
+            }
             availableMoves.push(checkingSquare)
         }
         for (let i = currentSquare.col + 1; i < 8; i++) {
             let checkingSquare = Square.at(currentSquare.row, i)
-            if (board.getPiece(checkingSquare) != undefined) break
+            if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+            if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                availableMoves.push(checkingSquare)
+                break
+            }
             availableMoves.push(checkingSquare)
         }
         for (let i = currentSquare.col - 1; i >= 0; i--) {
             let checkingSquare = Square.at(currentSquare.row, i)
-            if (board.getPiece(checkingSquare) != undefined) break
+            if (board.getPiece(checkingSquare) != undefined && (board.getPiece(checkingSquare)?.player == this.player || board.getPiece(checkingSquare) instanceof King)) break
+            if (board.getPiece(checkingSquare) != undefined && board.getPiece(checkingSquare)?.player != this.player) {
+                availableMoves.push(checkingSquare)
+                break
+            }
             availableMoves.push(checkingSquare)
         }
 
