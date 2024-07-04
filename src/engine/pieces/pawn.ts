364ad7034c35a04d14e46b1da575ca8c770587da
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-04 17:24:34 +0300

    refactoring done

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -9,89 +9,104 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    private moveInFront1or2(board: Board, availableMoves: any[], direction: string){
+        const currentSquare = board.findPiece(this);
+
+        const i1 = direction == 'up' ? 1 : -1;
+        const i2 = direction == 'up' ? 2 : -2;
+
+        let newAvailableMoves = availableMoves;
+
+        const pieceInFront1 = board.getPiece(Square.at(currentSquare.row + i1,currentSquare.col));
+        const pieceInFront2 = board.getPiece(Square.at(currentSquare.row + i2,currentSquare.col));
+
+        // moving one space in front, avoiding the last row
+        if (pieceInFront1 == undefined) {
+            availableMoves.push(Square.at(currentSquare.row + i1, currentSquare.col));
+        }
+
+        // moving two spaces in front, only on the first move
+        if ((direction == 'up' ? (currentSquare.row == 1) : (currentSquare.row == 6)) && pieceInFront1 == undefined && pieceInFront2 == undefined) {
+            availableMoves.push(Square.at(currentSquare.row + i2, currentSquare.col))
+        }
+
+        return newAvailableMoves;
+    }
+
+    private moveDiagonallyToTake(board: Board, availableMoves: any[], rowDirection: string, colDirection: string) {
+        const currentSquare = board.findPiece(this);
+
+        const iRow = rowDirection == 'up' ? 1 : -1;
+        const iCol = colDirection == 'right' ? 1 : -1;
+
+        let newAvailableMoves = availableMoves;
+
+        if (colDirection == 'right' ? (currentSquare.col + 1 < 8) : (currentSquare.col - 1 >= 0)) {
+            const pieceDiagonally = board.getPiece(Square.at(currentSquare.row + iRow,currentSquare.col + iCol));
+            if(pieceDiagonally != undefined && pieceDiagonally?.player != this.player && !(pieceDiagonally instanceof King)) {
+                availableMoves.push(Square.at(currentSquare.row + iRow,currentSquare.col + iCol));
+            }
+        }
+
+        return newAvailableMoves;
+    }
+
+    private moveDiagonallyForEnPassant(board: Board, availableMoves: any[], rowDirection: string, colDirection: string) {
+        const currentSquare = board.findPiece(this);
+
+        const iRow = rowDirection == 'up' ? 1 : -1;
+        const iCol = colDirection == 'right' ? 1 : -1;
+
+        let newAvailableMoves = availableMoves;
+
+        if (colDirection == 'right' ? (currentSquare.col + 1 < 8) : (currentSquare.col - 1 >= 0)) {
+            let pieceOnTheSide = board.getPiece(Square.at(currentSquare.row,currentSquare.col + iCol));
+            if(pieceOnTheSide != undefined && pieceOnTheSide?.player != this.player && pieceOnTheSide instanceof Pawn) {
+                let enPassantLastMove = {
+                    oldSquare: Square.at(currentSquare.row + iRow * 2,currentSquare.col + iCol),
+                    newSquare: Square.at(currentSquare.row,currentSquare.col + iCol)
+                }
+                if (board.lastMove.oldSquare?.equals(enPassantLastMove.oldSquare) && board.lastMove.newSquare?.equals(enPassantLastMove.newSquare)){
+                    availableMoves.push(Square.at(currentSquare.row + iRow,currentSquare.col + iCol))
+                }
+            }
+        }
+
+        return newAvailableMoves;
+    }
+
     public getAvailableMoves(board: Board) {
         let currentSquare = board.findPiece(this)
-        let availableMoves = new Array(0)
+        let availableMoves = new Array(0);
 
+        // white pawn
         if (this.player == Player.WHITE && currentSquare.row < 7) {
-            let pieceInFront1 = board.getPiece(Square.at(currentSquare.row + 1,currentSquare.col));
-            let pieceInFront2 = board.getPiece(Square.at(currentSquare.row + 2,currentSquare.col));
-            if (pieceInFront1 == undefined) availableMoves.push(Square.at(currentSquare.row + 1, currentSquare.col))
-            if (currentSquare.row == 1 && pieceInFront1 == undefined && pieceInFront2 == undefined) availableMoves.push(Square.at(currentSquare.row + 2, currentSquare.col))
+            // move
+            availableMoves = this.moveInFront1or2(board, availableMoves, 'up');
 
-            if (currentSquare.col + 1 < 8) {
-                let pieceDiagonallyRight = board.getPiece(Square.at(currentSquare.row + 1,currentSquare.col + 1));
-                if(pieceDiagonallyRight != undefined && pieceDiagonallyRight?.player != this.player && !(pieceDiagonallyRight instanceof King)) availableMoves.push(Square.at(currentSquare.row + 1,currentSquare.col + 1))
-            }
-            if (currentSquare.col - 1 >= 0) {
-                let pieceDiagonallyLeft = board.getPiece(Square.at(currentSquare.row + 1,currentSquare.col - 1));
-                if(pieceDiagonallyLeft != undefined && pieceDiagonallyLeft?.player != this.player && !(pieceDiagonallyLeft instanceof King)) availableMoves.push(Square.at(currentSquare.row + 1,currentSquare.col - 1))
-            }
+            // take
+            availableMoves = this.moveDiagonallyToTake(board, availableMoves, 'up', 'right');
+            availableMoves = this.moveDiagonallyToTake(board, availableMoves, 'up', 'left');
 
-            if (currentSquare.col + 1 < 8) {
-                let pieceToTheRight = board.getPiece(Square.at(currentSquare.row,currentSquare.col + 1));
-                if(pieceToTheRight != undefined && pieceToTheRight?.player != this.player && pieceToTheRight instanceof Pawn) {
-                    let enPassantLastMove = {
-                        oldSquare: Square.at(currentSquare.row + 2,currentSquare.col + 1),
-                        newSquare: Square.at(currentSquare.row,currentSquare.col + 1)
-                    }
-                    if (board.lastMove.oldSquare?.equals(enPassantLastMove.oldSquare) && board.lastMove.newSquare?.equals(enPassantLastMove.newSquare)){
-                        availableMoves.push(Square.at(currentSquare.row + 1,currentSquare.col + 1))
-                    }
-                }
-            }
-            if (currentSquare.col - 1 >= 0) {
-                let pieceToTheLeft = board.getPiece(Square.at(currentSquare.row,currentSquare.col - 1));
-                if(pieceToTheLeft != undefined && pieceToTheLeft?.player != this.player && pieceToTheLeft instanceof Pawn) {
-                    let enPassantLastMove = {
-                        oldSquare: Square.at(currentSquare.row + 2,currentSquare.col - 1),
-                        newSquare: Square.at(currentSquare.row,currentSquare.col - 1)
-                    }
-                    if (board.lastMove.oldSquare?.equals(enPassantLastMove.oldSquare) && board.lastMove.newSquare?.equals(enPassantLastMove.newSquare)){
-                        availableMoves.push(Square.at(currentSquare.row + 1,currentSquare.col - 1))
-                    }
-                }
-            }
+            // en passant
+            availableMoves = this.moveDiagonallyForEnPassant(board, availableMoves, 'up', 'right');
+            availableMoves = this.moveDiagonallyForEnPassant(board, availableMoves, 'up', 'left');
         }
+
+        // black pawn
         if (this.player == Player.BLACK && currentSquare.row > 0) {
-            let pieceInFront1 = board.getPiece(Square.at(currentSquare.row - 1,currentSquare.col));
-            let pieceInFront2 = board.getPiece(Square.at(currentSquare.row - 2,currentSquare.col));
-            if (pieceInFront1 == undefined)  availableMoves.push(Square.at(currentSquare.row - 1, currentSquare.col))
-            if (currentSquare.row == 6 && pieceInFront1 == undefined && pieceInFront2 == undefined) availableMoves.push(Square.at(currentSquare.row - 2, currentSquare.col))
+            // move
+            availableMoves = this.moveInFront1or2(board, availableMoves, 'down');
 
-            if (currentSquare.col + 1 < 8) {
-                let pieceDiagonallyRight = board.getPiece(Square.at(currentSquare.row - 1,currentSquare.col + 1));
-                if(pieceDiagonallyRight != undefined && pieceDiagonallyRight?.player != this.player && !(pieceDiagonallyRight instanceof King)) availableMoves.push(Square.at(currentSquare.row - 1,currentSquare.col + 1))
-            }
-            if (currentSquare.col - 1 >= 0) {
-                let pieceDiagonallyLeft = board.getPiece(Square.at(currentSquare.row - 1,currentSquare.col - 1));
-                if(pieceDiagonallyLeft != undefined && pieceDiagonallyLeft?.player != this.player && !(pieceDiagonallyLeft instanceof King)) availableMoves.push(Square.at(currentSquare.row - 1,currentSquare.col - 1))
-            }
+            // take
+            availableMoves = this.moveDiagonallyToTake(board, availableMoves, 'down', 'right');
+            availableMoves = this.moveDiagonallyToTake(board, availableMoves, 'down', 'left');
 
-            if (currentSquare.col + 1 < 8) {
-                let pieceToTheRight = board.getPiece(Square.at(currentSquare.row,currentSquare.col + 1));
-                if(pieceToTheRight != undefined && pieceToTheRight?.player != this.player && pieceToTheRight instanceof Pawn) {
-                    let enPassantLastMove = {
-                        oldSquare: Square.at(currentSquare.row - 2,currentSquare.col + 1),
-                        newSquare: Square.at(currentSquare.row,currentSquare.col + 1)
-                    }
-                    if (board.lastMove.oldSquare?.equals(enPassantLastMove.oldSquare) && board.lastMove.newSquare?.equals(enPassantLastMove.newSquare)){
-                        availableMoves.push(Square.at(currentSquare.row - 1,currentSquare.col + 1))
-                    }
-                }
-            }
-            if (currentSquare.col - 1 >= 0) {
-                let pieceToTheLeft = board.getPiece(Square.at(currentSquare.row,currentSquare.col - 1));
-                if(pieceToTheLeft != undefined && pieceToTheLeft?.player != this.player && pieceToTheLeft instanceof Pawn) {
-                    let enPassantLastMove = {
-                        oldSquare: Square.at(currentSquare.row - 2,currentSquare.col - 1),
-                        newSquare: Square.at(currentSquare.row,currentSquare.col - 1)
-                    }
-                    if (board.lastMove.oldSquare?.equals(enPassantLastMove.oldSquare) && board.lastMove.newSquare?.equals(enPassantLastMove.newSquare)){
-                        availableMoves.push(Square.at(currentSquare.row - 1,currentSquare.col - 1))
-                    }
-                }              }
+            // en passant
+            availableMoves = this.moveDiagonallyForEnPassant(board, availableMoves, 'down', 'right');
+            availableMoves = this.moveDiagonallyForEnPassant(board, availableMoves, 'down', 'left');
         }
+
         return availableMoves;
     }
 }
